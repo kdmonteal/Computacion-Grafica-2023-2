c4f457b6f36485118f6e832f76f89ebde09a587a
Daniella Marin <kdmonteal@gmail.com>
2023-09-27 09:40:06 -0500

    GLTF working

--- loadModels/src/js/mainObjects.js
+++ loadModels/src/js/mainObjects.js
@@ -16,10 +16,10 @@ function startScene() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000); //33FFC5
     camera = new THREE.PerspectiveCamera(
-                                            75,                                        //Angulo de visión(Abajo o arriba) 
-                                            window.innerWidth / window.innerHeight,    //Relación de aspecto 16:9
-                                            0.1,                                       //Mas cerca (no renderiza)
-                                            1000);                                    //Mas lejos ()
+        75,                                        //Angulo de visión(Abajo o arriba) 
+        window.innerWidth / window.innerHeight,    //Relación de aspecto 16:9
+        0.1,                                       //Mas cerca (no renderiza)
+        1000);                                    //Mas lejos ()
 
     renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('modelsLoad') });
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -38,8 +38,8 @@ function startScene() {
     // const axesHelper = new THREE.AxesHelper(5);
     // scene.add(axesHelper);
 
-    const lightAmbient = new THREE.AmbientLight( 0xFFFFFF ); // soft white light
-    scene.add( lightAmbient );
+    const lightAmbient = new THREE.AmbientLight(0xFFFFFF); // soft white light
+    scene.add(lightAmbient);
 
     // const light = new THREE.PointLight( 0xffffff, 1, 100 );
     // light.position.set( 5,10,10 );
@@ -47,24 +47,25 @@ function startScene() {
 
     animate();
     // Escenario
-    loadModel_objMtl("../src/models/obj_mtl/escenario/","escenario.obj","escenario.mtl");
+    loadModel_objMtl("../src/models/obj_mtl/escenario/", "escenario.obj", "escenario.mtl");
     // Human Model
-    loadModel_objMtl("../src/models/obj_mtl/personaje/","personaje.obj","personaje.mtl");
-
+    loadModel_objMtl("../src/models/obj_mtl/personaje/", "personaje.obj", "personaje.mtl");
+    // Duck Model
+    loadGltf('../src/models/gltf/pato/', 'Duck.gltf');
 }
 
-function animate(){
+function animate() {
     requestAnimationFrame(animate);
     controls.update();
     renderer.render(scene, camera);
 }
 
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener('resize', onWindowResize, false);
 
-function onWindowResize(){
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function loadModel_objMtl(path, nameObj, nameMtl) {
@@ -81,7 +82,52 @@ function loadModel_objMtl(path, nameObj, nameMtl) {
         objLoader.setMaterials(materials);
         objLoader.load(nameObj, function (object) {
             scene.add(object);
-            object.scale.set(2,2,2);
+            object.scale.set(2, 2, 2);
         });
     });
+}
+
+function loadGltf(path, nameGltfGet) {
+    var nameGltf = path + nameGltfGet;
+
+    alert(nameGltf);
+    // Instantiate a loader
+    const loader = new THREE.GLTFLoader();
+
+    // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+    const dracoLoader = new THREE.DRACOLoader();
+    dracoLoader.setDecoderPath(path);
+    loader.setDRACOLoader(dracoLoader);
+
+    // Load a glTF resource
+    loader.load(
+        // resource URL
+        nameGltf,
+        // called when the resource is loaded
+        function (gltf) {
+
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene; // THREE.Group
+            gltf.scenes; // Array<THREE.Group>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+            gltf.scene.position.set(0, 0, 2);
+
+        },
+        // called while loading is progressing
+        function (xhr) {
+
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+        },
+        // called when loading has errors
+        function (error) {
+
+            console.log('An error happened');
+
+        }
+    );
 }
